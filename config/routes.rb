3fe1a6734bceb5e7b6895d522a1caba0a857fbd8
Victor Mours <victor.mours@gmail.com>
2018-08-17 12:45:01 +0200

    Fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :clients do
-    member do
-      resources :events, only: [:new, :create]
-    end
+    resources :events, only: [:new, :create]
   end
   resources :events, except: [:new, :create]
   resources :rooms
