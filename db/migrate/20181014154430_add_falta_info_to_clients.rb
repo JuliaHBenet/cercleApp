fde4ae6a13ada4046f824035d0018dce5b5cdb0c
Victor Mours <victor.mours@gmail.com>
2018-10-14 18:52:59 +0200

    Make sure that falta_info is false

--- db/migrate/20181014154430_add_falta_info_to_clients.rb
+++ db/migrate/20181014154430_add_falta_info_to_clients.rb
@@ -1,5 +1,5 @@
 class AddFaltaInfoToClients < ActiveRecord::Migration[5.1]
   def change
-    add_column :clients, :falta_info, :boolean
+    add_column :clients, :falta_info, :boolean, default: false
   end
 end
